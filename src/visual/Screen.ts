f11451ce216e2d562bc716053a3d1e17db3843d7
floffah <therealfloffah@gmail.com>
2020-12-23 14:11:02 +0000

    sort-of works but broken on cmd tab

--- src/visual/Screen.ts
+++ src/visual/Screen.ts
@@ -1,5 +1,6 @@
 import App from "../App";
 import {terminal, Terminal} from "terminal-kit";
+import {spawn} from "node-pty";
 
 export default class Screen {
     app: App;
@@ -18,44 +19,83 @@ export default class Screen {
             logs: ["^BWelcome!^:", "^CKeybinds:^:", "^CCTRL_C^ - exit", "^CCTRL_N/CTRL_RIGHT^ - next tab", "^CCTRL_P/CTRL_LEFT^ - previous tab", "^CCTRL_A^ - new tab"]
         }]
     };
+    processes: ProcessState[] = []
 
     start() {
         this.render();
-
+        // console.log(JSON.stringify(process.env, null, 2));
         process.on("SIGINT", () => this.shutdown(false));
         this.term.on("key", (a: string, b: string[], c: { isCharacter: boolean; codepoint: number; code: number | Buffer; }) => this.key(a, b, c));
         this.term.on("resize", (w: number, h: number) => this.resize(w, h));
         this.term.grabInput(true);
     }
 
-    key(name: string, _matches: string[], _data: { isCharacter: boolean, codepoint: number, code: number | Buffer }) {
+    key(name: string, _matches: string[], data: { isCharacter: boolean, codepoint: number, code: number | Buffer }) {
         if (name === "CTRL_C") {
             this.shutdown(false);
-        } else if(name === "CTRL_N" || name === "CTRL_RIGHT") {
-            if(!!this.state.tabs[this.state.tab+1]) {
+        } else if (name === "CTRL_N" || name === "CTRL_RIGHT") {
+            if (!!this.state.tabs[this.state.tab + 1]) {
                 this.state.tab += 1;
                 this.render(true, false);
             }
-        } else if(name === "CTRL_P" || name === "CTRL_LEFT") {
-            if(!!this.state.tabs[this.state.tab-1]) {
+        } else if (name === "CTRL_P" || name === "CTRL_LEFT") {
+            if (!!this.state.tabs[this.state.tab - 1]) {
                 this.state.tab -= 1;
                 this.render(true, false);
             }
-        } else if(name === "CTRL_A") {
+        } else if (name === "CTRL_A") {
             this.newTab();
+        } else {
+            let tab = this.state.tabs[this.state.tab];
+            if ("proci" in tab) {
+                let proc = this.processes[tab.proci];
+                proc.keypress(name, data.codepoint, data.code);
+                //this.render();
+            }
         }
     }
 
     newTab() {
-        if(process.platform === "win32") {
-            this.state.tabs.push({
-                type: "proc",
-                title: "CMD",
-                proc: "cmd",
-                logs: ["^KStarting process..."]
-            });
-            this.render(true, false);
+        let title = "", proc = "";
+        if (process.platform === "win32") {
+            title = "CMD";
+            proc = "cmd.exe";
         }
+        this.state.tabs.push({
+            type: "proc",
+            title: title,
+            proc: proc,
+            logs: ["^KStarting process..."],
+            proci: this.processes.length,
+        });
+        let tab = this.state.tabs.length - 1;
+        this.processes.push({
+            command: proc,
+            tab,
+            keypress(_name, _codepoint, _code) {
+                //p.write(String.fromCharCode(codepoint))
+            }
+        });
+        let p = spawn(proc, [], {
+            cwd: process.cwd(),
+            cols: this.term.width,
+            rows: this.term.height - 2, //@ts-ignore
+            env: process.env,
+            useConpty: true,
+            encoding: "utf8",
+            name: "cmd"
+        });
+        p.onData((c) => {
+            this.logtab(tab, "^:" + c);
+            this.render();
+        })
+        process.stdin.on("data", (d: Buffer) => {
+            if(this.state.tab === tab) {
+                p.write(d.toString("utf-8"));
+                this.render();
+            }
+        });
+        this.render(true, false);
     }
 
     resize(_width: number, _height: number) {
@@ -109,6 +149,9 @@ export default class Screen {
 
     logtabline(tab: number, ...message: string[]) {
         this.state.tabs[tab].logs.push(message.join(" "));
+        if (this.state.tabs[tab].logs.length >= 49) {
+            this.state.tabs[tab].logs.shift();
+        }
     }
 
     log(...message: string[]) {
@@ -147,7 +190,7 @@ export default class Screen {
                 if (line.length > this.term.width) {
                     let amt = line.length / this.term.width;
                     for (let i = 0; i < amt; i++) {
-                        clines.push(line.substr(i * this.term.width, this.term.width));
+                        clines.push(line.substr(i * this.term.width, (i + 1) * this.term.width));
                     }
                     compensate += amt - 1;
                 } else {
@@ -178,6 +221,12 @@ export default class Screen {
     }
 }
 
+interface ProcessState {
+    command: string,
+    tab: number,
+    keypress: (name: string, codepoint: number, code: number | Buffer) => void
+}
+
 interface ScreenState {
     tab: number,
     tabs: ScreenTab[],
@@ -194,5 +243,6 @@ interface ProcessTab {
     type: "proc",
     title: string,
     proc: string,
-    logs: string[]
+    logs: string[],
+    proci: number,
 }
